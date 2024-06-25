85d563d5f69d5ef7fdafdf61a940c546a2a0486d
asegaf74 <asegaf8990@gmail.com>
2024-06-25 20:34:29 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exer2</title>
+</head>
+<body>
+    <header>
+        <h1>Multi-Blog Projects</h1>
+        <p>This is another exercise project by <b>Ace Auditores</b></p>
+        <hr>
+    </header>
+    <main>
+        <div>
+            <h2>This is the main page</h2>
+            <p>This is the descriptions</p>
+        </div>
+        <div>
+            <h2>This is the main page</h2>
+            <p>This is the descriptions</p>
+        </div>
+    </main>
+    <footer>
+        <hr>
+        <p>&copy; 2019-{{date('Y')}} Auditores Company</p>
+    </footer>
+</body>
+</html>
